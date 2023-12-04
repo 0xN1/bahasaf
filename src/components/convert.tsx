59618ad7356df915ddfe58da78761cbfe4a759b1
Nero One <info@nero1.net>
2023-12-04 16:07:19 +0800

    feat: :sparkles: convert “bahasa” h1 to input, update bahasa with router push, add new talk feature
    changed previously h1 to input and handle bahasa changes. add talk function to get data from google tts

--- src/components/convert.tsx
+++ src/components/convert.tsx
@@ -2,22 +2,35 @@
 
 import { encodeText } from "@/utils/convert";
 import { useEffect, useState } from "react";
-
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import toast, { Toaster } from "react-hot-toast";
+import getTTS from "@/action/get-tts";
 
 type Props = {};
 
 const ConvertText = (props: Props) => {
   const searchParams = useSearchParams();
-  const bahasa = searchParams.get("bahasa") || "f";
+  const router = useRouter();
+  const bahasa = searchParams.get("bahasa") || "";
 
   const [text, setText] = useState("");
   const [encoded, setEncoded] = useState("");
+  const [change, setChange] = useState(false);
 
   const encode = (text: string) => {
     const res = encodeText(text, bahasa);
     setEncoded(res);
+  };
+
+  const changeBahasa = (e: React.ChangeEvent<HTMLInputElement> | string) => {
+    if (typeof e === "string") {
+      setChange(true);
+      router.push(`/?bahasa=${e}`);
+      return;
+    }
+    const bahasa = e.target.value;
+    setChange(true);
+    router.push(`/?bahasa=${bahasa}`);
   };
 
   const saveToClipboard = () => {
@@ -33,6 +46,29 @@ const ConvertText = (props: Props) => {
     ));
   };
 
+  // const speech = new SpeechSynthesisUtterance();
+  // speech.lang = "ms-MY";
+  // speech.voice = speechSynthesis.getVoices()[6];
+  // speech.text = encoded;
+  // window.speechSynthesis.speak(speech);
+
+  const talk = async () => {
+    const origin =
+      typeof window !== "undefined" && window.location.origin
+        ? window.location.origin
+        : "";
+
+    if (!origin) return;
+    const data = await getTTS(encoded, origin);
+
+    if (!data.body) return null;
+    const { audioContent } = data.body;
+
+    const audio = new Audio();
+    audio.src = "data:audio/mpeg;base64," + audioContent;
+    audio.play();
+  };
+
   useEffect(() => {
     if (!text) return;
     if (text.length > 100) return;
@@ -40,11 +76,30 @@ const ConvertText = (props: Props) => {
     encode(text);
   }, [text]);
 
+  useEffect(() => {
+    if (change) {
+      encode(text);
+      setChange(false);
+    }
+  }, [bahasa]);
+
+  useEffect(() => {
+    changeBahasa("f");
+  }, []);
+
   return (
     <div className="w-full max-w-prose items-center justify-center p-2 sm:p-8">
-      <h1 className="mb-16 w-full text-center font-gmono text-8xl font-semibold">
-        {bahasa}
-      </h1>
+      <input
+        max={3}
+        min={1}
+        value={bahasa}
+        placeholder="f"
+        onChange={(e) => {
+          setChange(true);
+          changeBahasa(e);
+        }}
+        className="duration-250 focus:ring-none pointer-events-auto h-full w-full bg-transparent text-center font-gmono text-8xl text-orange-100 transition-all ease-linear selection:bg-zinc-800 placeholder:text-orange-300 focus:outline-none"
+      ></input>
       <div className="w-full items-center justify-center text-center">
         <input
           autoComplete="off"
@@ -55,20 +110,33 @@ const ConvertText = (props: Props) => {
           placeholder="tulis sini"
           value={text}
           onChange={(e) => setText(e.target.value)}
+          onSubmit={(e) => e.preventDefault()}
         ></input>
-        <div
-          className="peer mt-8 w-full max-w-prose text-center text-4xl lowercase transition-all duration-150 ease-in active:text-orange-400 sm:text-5xl "
-          onClick={(e) => saveToClipboard()}
-        >
-          {encoded}
-        </div>
 
-        <button
-          onClick={(e) => saveToClipboard()}
-          className="visible mt-8 w-32 rounded-lg p-2 text-center ring-1 ring-orange-200 hover:bg-orange-400 active:bg-orange-400 peer-empty:invisible"
-        >
-          copy text
-        </button>
+        {text && text.length > 0 && (
+          <div>
+            <div
+              className="peer mt-8 w-full max-w-prose text-center text-4xl lowercase transition-all duration-150 ease-in active:text-orange-400 sm:text-5xl "
+              onClick={(e) => saveToClipboard()}
+            >
+              {encoded}
+            </div>
+            <div className="flex w-full flex-col items-center justify-center sm:flex-row sm:gap-4">
+              <button
+                onClick={(e) => saveToClipboard()}
+                className="visible mt-8 w-32 rounded-lg p-2 text-center ring-1 ring-orange-200 hover:bg-orange-400 active:bg-orange-400 peer-empty:invisible"
+              >
+                copy text
+              </button>
+              <button
+                onClick={talk}
+                className="visible mt-8 w-32 rounded-lg p-2 text-center ring-1 ring-orange-200 hover:bg-orange-400 active:bg-orange-400 peer-empty:invisible"
+              >
+                talk
+              </button>
+            </div>
+          </div>
+        )}
 
         <Toaster />
       </div>
